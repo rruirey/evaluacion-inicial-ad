9f12762fcbb3e63f37e9a3efc9cca87a94966bf9
rruirey <rruirey2301@g.educaand.es>
2022-09-29 10:40:14 +0200

    small changes.

--- actividad-2/src/main/java/es/educaand/evaluacion/actividad1/DNIUtils.java
+++ actividad-2/src/main/java/es/educaand/evaluacion/actividad1/DNIUtils.java
@@ -7,16 +7,16 @@ public final class DNIUtils {
   private static final Random RANDOM = new Random();
 
   /**
-   * Represents the table that the DNI algorithm follows
-   * to generate the letter.
+   * Represents the table char that the DNI algorithm follows
+   * to generate the char.
    */
-  private static final String[] LETTER_TABLE = new String[]{
-      "T", "R", "W", "A",
-      "G", "M", "Y", "F",
-      "P", "D", "X", "B",
-      "N", "J", "Z", "S",
-      "Q", "V", "H", "L",
-      "C", "K", "E"
+  private static final char[] CHAR_TABLE = new char[]{
+      'T', 'R', 'W', 'A',
+      'G', 'M', 'Y', 'F',
+      'P', 'D', 'X', 'B',
+      'N', 'J', 'Z', 'S',
+      'Q', 'V', 'H', 'L',
+      'C', 'K', 'E'
   };
 
   /**
@@ -33,10 +33,10 @@ public final class DNIUtils {
   public static String generateDNI() {
     int number = 0;
     for (int i = 0; i <= DNI_DIGITS; i++) {
-      number += RANDOM.nextInt(DNI_DIGITS + 1) * Math.pow(10, i);
+      number += RANDOM.nextInt(10) * Math.pow(10, i);
     }
 
     final int mod = number % 23;
-    return number + LETTER_TABLE[mod];
+    return number + String.valueOf(CHAR_TABLE[mod]);
   }
 }
